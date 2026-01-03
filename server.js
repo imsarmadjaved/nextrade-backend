0a647871018db99f4ae370861c7a302fd15a5e55
sarmad153 <sarmadofficial.6@gmail.com>
2026-01-03 17:03:22 +0500

    Fixes

--- server.js
+++ server.js
@@ -69,7 +69,8 @@ app.use('/api/contact', contactRoutes); //customer support
 
 // image uploading
 app.use("/api/upload", uploadRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static("/mnt/data/uploads"));
+
 
 // Test route
 app.get("/", (req, res) => {
