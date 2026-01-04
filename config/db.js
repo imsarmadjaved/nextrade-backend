ad168c30dcf8c4aba8b48d08aff76a31a52d6d2b
sarmad153 <sarmadofficial.6@gmail.com>
2026-01-04 23:24:26 +0500

    fix

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        process.exit(1); 
-    }
+const connectDB = () => {
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => {
+            console.log("MongoDB connected");
+        })
+        .catch((err) => {
+            console.error("MongoDB connection error:", err.message);
+        });
 };
 
 module.exports = connectDB;
