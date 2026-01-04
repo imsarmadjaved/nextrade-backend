842fad8cb718fda631a2ab93fd679b1e16bedc92
sarmad153 <sarmadofficial.6@gmail.com>
2026-01-04 23:51:38 +0500

    fix

--- server.js
+++ server.js
@@ -70,5 +70,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
