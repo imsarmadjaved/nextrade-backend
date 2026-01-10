9dfc9f9bcc280957cfae19b45e220eba3900f277
sarmad153 <sarmadofficial.6@gmail.com>
2026-01-10 22:47:12 +0500

    fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-    origin: true,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "User-Fingerprint"],
     credentials: true
